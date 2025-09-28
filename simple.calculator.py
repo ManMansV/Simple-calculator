2ce49b6f8c16e60a9e35f1924ec3c5f45f6021e0
Manny <mannybeneda@gmail.com>
2025-09-27 22:18:47 -0600

    simple.calculator.py

--- simple.calculator.py
+++ simple.calculator.py
@@ -1,12 +1,12 @@
-def add (x,y):
+def add (x, y):
     return x + y
-def subtract(x,y):
+def subtract (x, y):
     return x - y
-def multiply(x, y):
+def multiply (x, y):
     return x * y
-def divide(x, y):
+def divide (x, y):
     if y == 0:
-        print("Error! Divide by zero.")
+        print("Error! Cannot divide by 0.")
     return x / y
 
 print("Choose an Operation")
@@ -15,10 +15,15 @@ print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 
-choice = input("Enter in a choice 1 - 4: ")
+choice = ''
 
-num1 = float(input("Enter in first number: "))
-num2 = float(input("Enter in second number: "))
+while choice not in ['1', '2', '3', '4']:
+    choice = input("Enter a choice 1 - 4: ")
+    if choice not in ['1', '2', '3', '4']:
+        print("Invalid choice! Try again.")
+
+num1 = float(input("Enter in a number: "))
+num2 = float(input("Enter in a number: "))
 
 if choice == '1':
     print("Results: ", add(num1, num2))
@@ -26,6 +31,7 @@ elif choice == '2':
     print("Results: ", subtract(num1, num2))
 elif choice == '3':
     print("Results: ", multiply(num1, num2))
-elif choice == '2':
+elif choice == '4':
     print("Results: ", divide(num1, num2))
-
+else:
+    print("Incorrect input! Try again.")
